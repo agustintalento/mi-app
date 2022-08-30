fe0684a7f1ea74d1b4a15293d9fc62d6058f6ea6
agustalento <agust193@gmail.com>
2022-08-29 22:53:13 -0300

    agrego estilos a navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './estilos.css'
 import NavBar from './components/NavBar';
 
 function App() {
